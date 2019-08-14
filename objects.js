d9351b757cb005eec17f8382c9a8be9989afb8c2
Nivetha <nivedha.orton@gmail.com>
2019-08-14 05:55:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Bts:"boy in love",exo:"loveshot",blackpink:"kill this love"}
 
 function updatePlaylist(playlist,artistName,songTitle){
-  playlist.artistName = songTitle;
-  return playlist
+  return playlist.artistName = songTitle;
+  
 }
